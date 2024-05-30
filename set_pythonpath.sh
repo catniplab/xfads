adf997a1cfcd1fa93545646404cf6db152a9e6c8
mahmoud-elmakki <mahmoudsmahdi7@gmail.com>
2024-05-30 22:22:49 +0100

    bash script to auto set env's PYTHONPATH to the project dir everytime the env is activated (pyproject.toml doesn't work with jupyter)

---
+++ set_pythonpath.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#Make the ecript executable 
+chmod +x set_pythonpath.sh
+
+# Get the environment name (already specified in environment.yaml)
+ENV_NAME=$(grep '^name:' environment.yaml | awk '{print $2}')
+
+# Get the directory of the specified environment
+ENV_PATH=$(conda env list | grep "${ENV_NAME}" | awk '{print $3}')
+POStACTIVATION_PATH=$ENV_PATH/etc/conda/activate.d
+
+# Print the environment path
+echo "The path to the environment '$ENV_NAME' is: $ENV_PATH"
+
+# Create a directory for the activation script
+mkdir -p $ENV_PATH/etc/conda/activate.d
+
+# Get the current directory using $PWD
+PROJECT_DIR="$PWD"
+
+# Assign the value of PROJECT_DIR to a txt file
+echo "$PROJECT_DIR" > $POStACTIVATION_PATH/project_dir.txt
+
+# Create the post-activation script
+echo 'PYTHONPATH=$(cat "$(dirname "$0")/project_dir.txt")'> $ENV_PATH/etc/conda/activate.d/set_pythonpath.sh
